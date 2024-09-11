f816c5514360e8788876e086fc9a0c955b1f24c4
0Notak <134090193+0Notak@users.noreply.github.com>
2024-09-10 23:42:27 -0600

    v2

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
-spark-submit \
+archivo_python=$1  
+argumento=$2
+
+spark-submit $archivo_python $argumento \
 
 # Ejemplo
   --class org.apache.spark.examples.SparkPi \
   --master yarn \
   --deploy-mode cluster \
-  --conf spark.sql.shuffle.partitions=500 \
-  --conf spark.executor.cores=4 \
-  --conf spark.executor.memory=8g \
-  --conf spark.driver.cores=2 \
-  --conf spark.driver.memory=4g \
+  --conf spark.sql.shuffle.partitions=200 \
+  --conf spark.executor.cores=8 \
+  --conf spark.executor.memory=32g \
+  --conf spark.driver.cores=32 \
+  --conf spark.driver.memory=128g \
 
   #Ejemplo
 --files /path/to/tabla.json
