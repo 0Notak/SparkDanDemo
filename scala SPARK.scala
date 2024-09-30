72455e77b3c9ec18f03e496ab8a289bb7f8e5253
HEVERT  DANIEL MARTINEZ <134090193+0Notak@users.noreply.github.com>
2024-09-30 15:35:27 -0600

    Update scala SPARK.scala

--- scala SPARK.scala
+++ scala SPARK.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.DataFrame
 
 // COMMAND ----------
 
+// SELECCION DE PATH PARA TRABAJAR CON MULTIPLES CSV CON POO
 
 class NDataframe(spark: SparkSession, inputPath: String) {
 
@@ -34,6 +35,8 @@ df.printSchema
 
 // COMMAND ----------
 
+// OPERARIONES Y FILTRADO DE COLUMNAS
+
 val df2 = df.select("canales_on_line", "panaderia")
 
 // COMMAND ----------
@@ -58,6 +61,9 @@ df2.select($"panaderia" / 1000).show()
 
 // COMMAND ----------
 
+// CAST DE COLUMNA a DATE
+
+
 var df_g=df.withColumn("fecha",col("indice_tiempo").cast("date"))
 df_g.printSchema()
 
@@ -67,16 +73,21 @@ val df_g2 = df_g.withColumn("anio", year($"fecha"))
 
 // COMMAND ----------
 
+// GROUPBY MAS OPERACIONES CORRESPONDIENTES
+
 val df0=df_g2.select("anio", "bebidas", "almacen", "panaderia", "lacteos", "carnes")
 df0.groupBy("anio").agg(sum("bebidas"),sum("almacen"), sum("panaderia"), sum("lacteos"), sum("carnes")).show()
 
 // COMMAND ----------
+
+// OPERACION ANTERIOS CON FOR 
 
 for (columns <- df0.columns) {
     df0.groupBy("anio").agg(sum(columns)).show()
 }
 
 // COMMAND ----------
+// USO DE udf y When y Otherwise
 
 val cambionombre = udf((panaderia: Int) => if (panaderia < 5000000) "Barato" else "Caro")
 val pan =df2.withColumn("panaderia Clasificacion", cambionombre($"panaderia"))
@@ -96,6 +107,8 @@ pan2.groupBy("When oth").count().show()
 
 // COMMAND ----------
 
+//Uso de Window
+
 import org.apache.spark.sql.expressions.Window
 
 val windowSpec = Window.partitionBy("anio").orderBy("panaderia")
@@ -112,6 +125,8 @@ fil.show()
 fil.withColumn("Fecha hoy", current_date).show()
 
 // COMMAND ----------
+
+//Creacion de vista temporal
 
 fil.createOrReplaceTempView("Fechahoy")
 
@@ -133,6 +148,8 @@ fil.printSchema()
 
 // COMMAND ----------
 
+// Cast de todas las columnas con SelectExpr
+
 val sel_tip=fil.selectExpr("ANIO", "CAST (bebidas AS FLOAT)", "CAST (bebidas AS FLOAT)", "CAST(almacen AS FLOAT)", "CAST (panaderia AS FLOAT)", "CAST (lacteos AS FLOAT)", "CAST(carnes AS FLOAT)", "Orden")
 
 // COMMAND ----------
@@ -144,6 +161,8 @@ sel_tip.printSchema()
 val table = "database.DANIEL"
 
 // COMMAND ----------
+
+// Creacion de tabla en SQL
 
 spark.sql("""" CREATE TABLE DANIEL IF NOT EXISTS (
   ANIO INT,
